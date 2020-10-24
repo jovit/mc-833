ceb3a08c054129af4606dc12422a94b944edb9b6
João Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2020-10-24 17:36:43 -0300

    Exerc2 1/2 done

--- Exercício 2/src/servidor.c
+++ Exercício 2/src/servidor.c
@@ -8,38 +8,48 @@
 #include <sys/socket.h>
 #include <time.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
 #define LISTENQ 10
 #define MAXDATASIZE 100
 
-int main (int argc, char **argv) {
-   int    listenfd, connfd;
+int main(int argc, char **argv)
+{
+   int listenfd, connfd;
    struct sockaddr_in servaddr;
-   char   buf[MAXDATASIZE];
+   char buf[MAXDATASIZE];
    time_t ticks;
 
-   if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+   if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+   {
       perror("socket");
       exit(1);
    }
 
    bzero(&servaddr, sizeof(servaddr));
-   servaddr.sin_family      = AF_INET;
-   servaddr.sin_addr.s_addr = htonl("192.168.0.16");
-   servaddr.sin_port        = htons(13);   
+   servaddr.sin_family = AF_INET;
 
-   if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
+   inet_pton(AF_INET, "127.0.0.1", &(servaddr.sin_addr));
+   // servaddr.sin_addr.s_addr = htonl("192.168.0.16");
+
+   servaddr.sin_port = htons(8585);
+
+   if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1)
+   {
       perror("bind");
       exit(1);
    }
-   
-   if (listen(listenfd, LISTENQ) == -1) {
+
+   if (listen(listenfd, LISTENQ) == -1)
+   {
       perror("listen");
       exit(1);
    }
 
-   for ( ; ; ) {
-      if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1 ) {
+   for (;;)
+   {
+      if ((connfd = accept(listenfd, (struct sockaddr *)NULL, NULL)) == -1)
+      {
          perror("accept");
          exit(1);
       }
@@ -50,5 +60,5 @@ int main (int argc, char **argv) {
 
       close(connfd);
    }
-   return(0);
+   return (0);
 }
