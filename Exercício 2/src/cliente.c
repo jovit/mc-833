0ce76424d188c0e89a1738a817b107b2c7217d9c
João Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2020-10-24 21:02:01 -0300

    pretty much all done ex2

--- Exercício 2/src/cliente.c
+++ Exercício 2/src/cliente.c
@@ -12,47 +12,72 @@
 
 #define MAXLINE 4096
 
-int main(int argc, char **argv) {
-   int    sockfd, n;
-   char   recvline[MAXLINE + 1];
-   char   error[MAXLINE + 1];
+int main(int argc, char **argv)
+{
+   int sockfd, n;
+   char recvline[MAXLINE + 1];
+   char error[MAXLINE + 1];
+   char local_address[128];
    struct sockaddr_in servaddr;
+   struct sockaddr_in address;
+   socklen_t address_len = sizeof(address);
 
-   if (argc != 2) {
-      strcpy(error,"uso: ");
-      strcat(error,argv[0]);
-      strcat(error," <IPaddress>");
+   if (argc != 3)
+   { // make sure that the ip address was passed as an argument
+      strcpy(error, "uso: ");
+      strcat(error, argv[0]);
+      strcat(error, " <IPaddress> <Port>");
       perror(error);
       exit(1);
    }
 
-   if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+   { // create socket
       perror("socket error");
       exit(1);
    }
 
    bzero(&servaddr, sizeof(servaddr));
-   servaddr.sin_family = AF_INET;
-   servaddr.sin_port   = htons(8585);
-   if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+   servaddr.sin_family = AF_INET;            // use IPv4
+   servaddr.sin_port = htons(atoi(argv[2])); // server port
+   if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+   { // convert the argument ip to a binary representation an attribute it to the server
       perror("inet_pton error");
       exit(1);
    }
 
-   if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+   if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+   { // establish connection to server
       perror("connect error");
       exit(1);
    }
 
-   while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
+   if (getsockname(sockfd, (struct sockaddr *)&address, &address_len) < 0)
+   {
+      perror("getsockname");
+      exit(1);
+   }
+
+   if (inet_ntop(AF_INET, &(address.sin_addr), local_address, sizeof(local_address)) < 0)
+   {
+      perror("inet_ntop error");
+      exit(1);
+   }
+   printf("Local address = %s\n", local_address);
+   printf("Local port number = %d\n", ntohs(address.sin_port));
+
+   while ((n = read(sockfd, recvline, MAXLINE)) > 0)
+   { // read message line from server
       recvline[n] = 0;
-      if (fputs(recvline, stdout) == EOF) {
+      if (fputs(recvline, stdout) == EOF)
+      { // print the received message to stdout
          perror("fputs error");
          exit(1);
       }
    }
 
-   if (n < 0) {
+   if (n < 0)
+   { // if no lines are received from server
       perror("read error");
       exit(1);
    }
--- Exercício 2/src/servidor.c
+++ Exercício 2/src/servidor.c
@@ -13,6 +13,54 @@
 #define LISTENQ 10
 #define MAXDATASIZE 100
 
+void show_server_socket_info(int fd)
+{
+   struct sockaddr_in addr;
+   socklen_t len = sizeof(addr);
+   char address_name[128];
+
+   if (getsockname(fd, (struct sockaddr *)&addr, &len) < 0)
+   {
+      perror("getpeername");
+      exit(1);
+   }
+
+   if (inet_ntop(AF_INET, &(addr.sin_addr), address_name, sizeof(address_name)) < 0)
+   {
+      perror("inet_ntop error");
+      exit(1);
+   }
+
+   printf("\n*****************************\n");
+   printf("Server address = %s\n", address_name);
+   printf("Server port number = %d\n", ntohs(addr.sin_port));
+   printf("*****************************\n");
+}
+
+void show_peer_info(int fd)
+{
+   struct sockaddr_in addr;
+   socklen_t len = sizeof(addr);
+   char address_name[128];
+
+   if (getpeername(fd, (struct sockaddr *)&addr, &len) < 0)
+   {
+      perror("getpeername");
+      exit(1);
+   }
+
+   if (inet_ntop(AF_INET, &(addr.sin_addr), address_name, sizeof(address_name)) < 0)
+   {
+      perror("inet_ntop error");
+      exit(1);
+   }
+
+   printf("\n-----------------------------\n");
+   printf("Client address = %s\n", address_name);
+   printf("Client port number = %d\n", ntohs(addr.sin_port));
+   printf("-----------------------------\n");
+}
+
 int main(int argc, char **argv)
 {
    int listenfd, connfd;
@@ -20,45 +68,47 @@ int main(int argc, char **argv)
    char buf[MAXDATASIZE];
    time_t ticks;
 
-   if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+   if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) // create tcp ipv4 socket
    {
       perror("socket");
       exit(1);
    }
 
    bzero(&servaddr, sizeof(servaddr));
-   servaddr.sin_family = AF_INET;
+   servaddr.sin_family = AF_INET;                // IPv4
+   servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // accept conections from all IP addresses available
+   servaddr.sin_port = 0;                        // will be attributed a random port on bind
+   // servaddr.sin_port = htons(8585); // start server on fixed port
 
-   inet_pton(AF_INET, "127.0.0.1", &(servaddr.sin_addr));
-   // servaddr.sin_addr.s_addr = htonl("192.168.0.16");
-
-   servaddr.sin_port = htons(8585);
-
-   if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1)
+   if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) // bind address configuration do created socket
    {
       perror("bind");
       exit(1);
    }
 
-   if (listen(listenfd, LISTENQ) == -1)
+   if (listen(listenfd, LISTENQ) == -1) // put socket on passive mode to accept incoming connections
    {
       perror("listen");
       exit(1);
    }
 
-   for (;;)
+   show_server_socket_info(listenfd);
+
+   for (;;) // keep accepting connections as they come
    {
-      if ((connfd = accept(listenfd, (struct sockaddr *)NULL, NULL)) == -1)
+      if ((connfd = accept(listenfd, (struct sockaddr *)NULL, NULL)) == -1) // // accept pending connection
       {
          perror("accept");
          exit(1);
       }
 
-      ticks = time(NULL);
-      snprintf(buf, sizeof(buf), "%.24s\r\n", ctime(&ticks));
-      write(connfd, buf, strlen(buf));
+      show_peer_info(connfd);
+      ticks = time(NULL);                                     // get currenty timestamp
+      snprintf(buf, sizeof(buf), "%.24s\r\n", ctime(&ticks)); // write current timestamp string to buffer
+      write(connfd, buf, strlen(buf));                        // send buffer to the accepted socket connection
 
-      close(connfd);
+      close(connfd); // close connection
    }
+
    return (0);
 }
